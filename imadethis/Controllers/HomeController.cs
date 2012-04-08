57d627692d9fc21c418e4fe890a5889ebc248567
Christian Wattengård <cwattengard@gmail.com>
2012-04-08 19:21:16 +0200

    Test 1

---
+++ imadethis/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace imadethis
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View ();
+		}
+		
+	}
+}
+
